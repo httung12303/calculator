433a26a6b8e14dec8f90d5ffe15ee9defca2f4ac
httung12303 <blackzerob562@gmail.com>
2022-09-20 21:58:55 +0700

    Add simple math functions

---
+++ script.js
@@ -0,0 +1,35 @@
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            return "Invalid operator: " + operator;    
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if(b === 0) return "Divided by zero";
+    return a / b;
+}
