84bf36273b9ef26deaa438d4b9944135bc478166
httung12303 <blackzerob562@gmail.com>
2022-09-20 22:13:50 +0700

    Add display, clear and delete on click

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const display = document.getElementById('display');
+const mathButtons = document.querySelectorAll('.math-button');
+const clearButton = document.getElementById('clear-button');
+const deleteButton = document.getElementById('delete-button');
+let ans = 0;
+
+clearButton.addEventListener('click', () => display.textContent = '');
+deleteButton.addEventListener('click', () => display.textContent = display.textContent.slice(0, -1));
+mathButtons.forEach(button => {
+    if(!button.classList.contains('equal-button')){
+        button.addEventListener('click', displayMathButton);
+    }
+});
+
+function displayMathButton() {
+    display.textContent += this.value; 
+}
 function operate(a, b, operator) {
     switch (operator) {
         case '+':
@@ -6,7 +23,7 @@ function operate(a, b, operator) {
         case '-':
             return subtract(a, b);
             break;
-        case '*':
+        case 'x':
             return multiply(a, b);
             break;
         case '/':
@@ -16,19 +33,15 @@ function operate(a, b, operator) {
             return "Invalid operator: " + operator;    
     }
 }
-
 function add(a, b) {
     return a + b;
 }
-
 function substract(a, b) {
     return a - b;
 }
-
 function multiply(a, b) {
     return a * b;
 }
-
 function divide(a, b) {
     if(b === 0) return "Divided by zero";
     return a / b;
